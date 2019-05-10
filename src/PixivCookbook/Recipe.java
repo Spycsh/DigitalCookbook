b86467886f2b0dde6aaaeed421f7f4bc1125fc46
pyonkti <751069606@qq.com>
2019-05-10 10:30:42 +0200

    comments for class Recipe and Step

--- src/PixivCookbook/Recipe.java
+++ src/PixivCookbook/Recipe.java
@@ -7,13 +7,12 @@ import java.io.Serializable;
 import java.util.LinkedList;
 
 /**
+ * entity class recipe
  * @author Ling Wei
  *
  */
 public class Recipe implements Serializable{
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	private String recipeName;
 	private String cuisineName;
@@ -24,65 +23,126 @@ public class Recipe implements Serializable{
 	private LinkedList<Step> steps = new LinkedList<Step>();
 	private int countSteps = 0;
 	
+	/**
+	 * constructor for class Recipe
+	 * @param recipeName the name of the recipe
+	 * @param cuisineName the name of cuisine the recipe belongs to
+	 * @param numberOfEaters the number of people who would have the dish
+	 */
 	public Recipe(String recipeName,String cuisineName,int numberOfEaters) {
 		this.setRecipeName(recipeName);
 		this.setCuisineName(cuisineName);
 		this.setNumberOfEaters(numberOfEaters);
 	}
 	
+	/**
+	 * add one ingredient to the recipe
+	 * @param newIngredient new ingredient of the recipe
+	 */
 	public void addIngredient(Ingredient newIngredient) {
 		ingredients.add(newIngredient);
 	}
 	
-	public void addPreparationStep(String step) {
+	/**
+	 * add one step to the recipe
+	 * @param stepContent the step's content
+	 */
+	public void addPreparationStep(String stepContent) {
 		setCountSteps(getCountSteps() + 1);
-		steps.add(new Step(step,getCountSteps() ));
+		steps.add(new Step(stepContent,getCountSteps() ));
 	}
 
+	/**
+	 * @return the latest step's number
+	 */
 	public int getCountSteps() {
 		return countSteps;
 	}
 
+	/**
+	 * default setter for step's number
+	 * @param countSteps
+	 */
 	public void setCountSteps(int countSteps) {
 		this.countSteps = countSteps;
 	}
 
+	/**
+	 * default getter for the name of the recipe
+	 * @return the name of the recipe
+	 */
 	public String getRecipeName() {
 		return recipeName;
 	}
 
+	/**
+	 * default setter for the name of the recipe
+	 * @param recipeName input name for the recipe
+	 */
 	public void setRecipeName(String recipeName) {
 		this.recipeName = recipeName;
 	}
 
+	/**
+	 * default getter for the name of the cuisine
+	 * @return the name of the cuisine
+	 */
 	public String getCuisineName() {
 		return cuisineName;
 	}
 
+	/**
+	 * default setter for the name of the cuisine
+	 * @param cuisineName
+	 */
 	public void setCuisineName(String cuisineName) {
 		this.cuisineName = cuisineName;
 	}
 
+	/**
+	 * default getter for the number of the eaters
+	 * @return the number of the eaters
+	 */
 	public int getNumberOfEaters() {
 		return numberOfEaters;
 	}
 
+	/**
+	 * default setter for the number of the eaters
+	 * @param numberOfEaters the input number of eaters from the customer
+	 */
 	public void setNumberOfEaters(int numberOfEaters) {
 		this.numberOfEaters = numberOfEaters;
 	}
 
+	/**
+	 * default getter for the time needed for the preparation phase
+	 * @return the time needed for the preparation phase(in minutes)
+	 */
 	public int getPreparationTime() {
 		return preparationTime;
 	}
 
+	/**
+	 * default setter for the time needed for the preparation phase
+	 * @param preparationTime input time needed for the preparation phase
+	 */
 	public void setPreparationTime(int preparationTime) {
 		this.preparationTime = preparationTime;
 	}
 
+	/**
+	 * default getter for the time needed for the cooking phase
+	 * @return the time needed for the cooking phase
+	 */
 	public int getCookingTime() {
 		return cookingTime;
 	}
 
+	/**
+	 * default setter for the time needed for the cooking phase
+	 * @param cookingTime input time needed for the cooking phase
+	 */
 	public void setCookingTime(int cookingTime) {
 		this.cookingTime = cookingTime;
 	}
--- src/PixivCookbook/Step.java
+++ src/PixivCookbook/Step.java
@@ -15,18 +15,32 @@ public class Step implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
 	private String content;
 	private int stepNumber;
 	
+	/**
+	 * constructor for class Step
+	 * @param inputContent the content of the step
+	 * @param stepNumber the current sequence number of this step
+	 */
 	public Step(String inputContent, int stepNumber) {
 		this.setContent(inputContent);
 		this.stepNumber = stepNumber; 
 	}
 
+	/**
+	 * default getter for the content of the step
+	 * @return the content of the step
+	 */
 	public String getContent() {
 		return content;
 	}
 
+	/**
+	 * default setter for the content of the step
+	 * @param content input the content for the step
+	 */
 	public void setContent(String content) {
 		this.content = content;
 	}
